55dce8f7d30b5099663b4934c803e496f3f033f4
andreisoroka <unickpro@gmail.com>
2018-03-06 16:33:56 +0300

    [config] not loading all functions in lodash

--- config/index.js
+++ config/index.js
@@ -1,17 +1,17 @@
-const _ = require('lodash');
+const defaults = require('lodash/defaults');
 
 const config_production = require('./production.json');
 const config_development = require('./development.json');
 
 /**
- *
+ * Get config
  * @param {string} NODE_ENV
  */
 function get(NODE_ENV) {
   let config = config_production;
 
   if (NODE_ENV === 'development') {
-    config = _.defaults(config_development, config);
+    config = defaults(config_development, config);
   }
   return config;
 }
